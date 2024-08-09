061b74ac796ada9643648d0214356a162e1c9ad7
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2024-08-09 10:01:49 +0530

    Create embedding_layer.py

---
+++ embedding_layer.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class EmbeddingLayer(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super(EmbeddingLayer, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+    def forward(self, x):
+        return self.embedding(x)
